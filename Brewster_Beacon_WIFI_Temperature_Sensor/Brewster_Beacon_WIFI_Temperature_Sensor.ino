d3f0659427630be80f3f0af4293512fc418cd681
Pavol Fóra <pavolfora@gmail.com>
2021-01-11 20:33:08 +0100

    Basic settings

--- Brewster_Beacon_WIFI_Temperature_Sensor/Brewster_Beacon_WIFI_Temperature_Sensor.ino
+++ Brewster_Beacon_WIFI_Temperature_Sensor/Brewster_Beacon_WIFI_Temperature_Sensor.ino
@@ -1,7 +1,18 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 
+ESP8266WiFiMulti wifiMulti;
+
 void setup() {
+	// ------------------------ Wifi AP + Repeaters ------------------
+	wifiMulti.addAP("WIFI_4A", "behringer/*-+");
+	wifiMulti.addAP("WIFI_4A_CELL", "behringer/*-+");
+	wifiMulti.addAP("WIFI_4A_GRDN", "behringer/*-+");
+	// ---------------------------------------------------------------
+
+	WiFi.mode(WIFI_STA);
+
+	Serial.begin(115200);
 }
 
 void loop() {
